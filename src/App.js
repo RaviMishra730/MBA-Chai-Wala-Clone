a84e7a86daa57b95bc87fafbc88fb99f2080450b
Ravi <coderravi730@gmail.com>
2022-11-28 13:19:19 +0530

    footer section added

--- src/App.js
+++ src/App.js
@@ -2,15 +2,93 @@
 import "./Styles/App.scss"
 import "./Styles/IntroVideo.scss"
 import "./Styles/Section.scss"
+import "./Styles/Footer.scss"
+import "./Styles/MediaQuery.scss"
+
 // Importing image
 import freshTopicImg from "./assets/asset 0.png";
+import freshTopicImg2 from "./assets/asset 1.png";
+import tedTalksImg from "./assets/asset 2.gif";
+import franchiseImg from "./assets/asset 3.gif";
+import mapImg from "./assets/asset 4.png";
+import courseImg from "./assets/asset 5.png";
+import albumImg from "./assets/asset 6.gif";
+import baratImg from "./assets/asset 7.png";
+import chaiWalaImg from "./assets/asset 8.png";
+
 // jsx Files 
 import IntroVideo from "./Components/IntroVideo";
 import Section from "./Components/Section";
+import Footer from "./Components/Footer";
+
+import data from "./Data/data.json";
+
+// $yellow:#fff100;
+// $pink:#ed1e79;
+// $red:#d20120;
+// $yellow:#fff100;
+// $white:#fff;
+// $brown:#6d3d0f;
+
+const yellow = "#fff100", pink = "#ed1e79", white = "#fff",brown = "#6d3d0f"
+
 function App() {
+
+  const { freshTopic } = data;
+  const { freshTopic2} = data;
+  const { tedTalks } = data;
+  const { franchise } = data;
+  const { map } = data;
+  const { courses } = data;
+  const { album } = data;
+  const { barat } = data;
+  const { chaiwala } = data;
+  
   return <>
-    <IntroVideo/>
-    <Section h3={'lorem'} text="lorem" btnTxt={"lorem button"} imgsrc={freshTopicImg} />
+    <IntroVideo />
+    
+
+    {/* FreshTopic1  */}
+    
+    <Section h3={freshTopic.heading} text={freshTopic.text} btnTxt={freshTopic.btn} imgSrc={freshTopicImg} backgroundColor={pink} headingColor={yellow} textColor={yellow} btnBgColor={yellow} 
+      btnColor = {pink}
+    />
+    {/* FreshTopic2  */}
+    <Section h3={freshTopic2.heading} text={freshTopic2.text} btnTxt={freshTopic2.btn} imgSrc={freshTopicImg2} backgroundColor={pink} headingColor={yellow} textColor={yellow} btnBgColor={yellow} 
+      btnColor = {pink}
+    />
+
+    {/* TedTalks  */}
+
+    <Section h3={tedTalks.heading} text={tedTalks.text} btnTxt={tedTalks.btn} imgSrc={tedTalksImg} backgroundColor={yellow} headingColor={pink} textColor={pink} btnBgColor={yellow} 
+      btnColor = {pink}
+    />
+    {/* Frenchise  */}
+
+    <Section h3={franchise.heading} text={franchise.text} btnTxt={franchise.btn} imgSrc={franchiseImg} backgroundColor={white} headingColor={pink} textColor={brown} btnBgColor={brown} 
+      btnColor = {yellow}
+    />
+    {/* Map */}
+    <Section h3={map.heading} text={map.text} imgSrc={mapImg} backgroundColor={pink} headingColor={yellow} textColor={yellow} btnBgColor={brown} 
+      btnColor = {yellow} hasBtn = {false}
+    />
+    {/* Courses */}
+    <Section h3={courses.heading} text={courses.text} btnTxt = {courses.btn} imgSrc={courseImg} imgSize = {"30%"} backgroundColor={yellow} headingColor={pink} textColor={pink} btnBgColor={pink} 
+      btnColor = {yellow}
+    />
+    {/* Albums */}
+    <Section h3={album.heading} text={album.text} btnTxt = {album.btn} imgSrc={albumImg} backgroundColor={white} headingColor={pink} textColor={brown} btnBgColor={brown} 
+      btnColor = {yellow}
+    />
+    {/* Barat */}
+    <Section h3={barat.heading} text={barat.text} btnTxt = {barat.btn} imgSrc={baratImg} backgroundColor={pink} headingColor={yellow} textColor={yellow} btnBgColor={yellow} 
+      btnColor = {pink}
+    />
+    {/* ChaiWala */}
+    <Section h3={chaiwala.heading} text={chaiwala.text} btnTxt = {chaiwala.btn} imgSrc={chaiWalaImg} backgroundColor={white} headingColor={pink} textColor={brown} btnBgColor={brown} 
+      btnColor = {yellow}
+    />
+    <Footer/>
   </>
 }
 
